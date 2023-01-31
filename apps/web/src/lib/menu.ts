27a5936da9c82a3ccd80fe9243f51a5a7655c2d2
adipol1359 <adrian.polak20@gmail.com>
2023-01-31 19:13:45 +0100

    refactor(web): change variable name

--- apps/web/src/lib/menu.ts
+++ apps/web/src/lib/menu.ts
@@ -15,9 +15,9 @@ export const calculateMenuPosition = ({
 	buttonY: number;
 }>) => {
 	const positionX = buttonX - (menuWidth - buttonWidth) / 2;
-	const menuSize = positionX + menuWidth - screenWidth;
+	const menuPosition = positionX + menuWidth - screenWidth;
 	const x =
-		positionX < 0 ? 0 : menuSize > 0 ? screenWidth - menuWidth : positionX;
+		positionX < 0 ? 0 : menuPosition > 0 ? screenWidth - menuWidth : positionX;
 	const y = buttonY + buttonHeight + 6;
 
 	return { x, y };
