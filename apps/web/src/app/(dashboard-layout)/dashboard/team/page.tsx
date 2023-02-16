5b8a63ced1193c14900c891cb2cf5e45866df365
adipol1359 <adrian.polak20@gmail.com>
2023-02-16 13:53:26 +0100

    refactor(web): change page function name

--- apps/web/src/app/(dashboard-layout)/dashboard/team/page.tsx
+++ apps/web/src/app/(dashboard-layout)/dashboard/team/page.tsx
@@ -1,7 +1,7 @@
 import { PageTitle } from '@/components/PageTitle';
 import { TeamList } from '@/components/TeamList/TeamList';
 
-export default function DashboardTeamListPage() {
+export default function DashboardTeamsPage() {
 	return (
 		<>
 			<PageTitle>Teams</PageTitle>
