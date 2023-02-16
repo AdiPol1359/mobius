96865fab46d62b283d51c1fc8544d03aa3a20b6c
adipol1359 <adrian.polak20@gmail.com>
2023-02-16 14:28:58 +0100

    refactor(web): remove Array#from from team list

--- apps/web/src/components/TeamList/TeamList.tsx
+++ apps/web/src/components/TeamList/TeamList.tsx
@@ -2,10 +2,14 @@ import { Team } from './Team';
 
 export const TeamList = () => (
 	<ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
-		{Array.from({ length: 3 }).map((_, i) => (
-			<li key={i}>
-				<Team name="Lorem ipsum" />
-			</li>
-		))}
+		<li>
+			<Team name="Lorem ipsum" />
+		</li>
+		<li>
+			<Team name="Lorem ipsum" />
+		</li>
+		<li>
+			<Team name="Lorem ipsum" />
+		</li>
 	</ul>
 );
