f93d161b119890e3378cb62785364ee0d984004c
adipol1359 <adrian.polak20@gmail.com>
2023-02-16 17:03:55 +0100

    feat(web): improve a11y

--- apps/web/src/components/DashboardNavigation.tsx
+++ apps/web/src/components/DashboardNavigation.tsx
@@ -11,22 +11,27 @@ import { ActiveLink } from '@/components/ActiveLink';
 const items = [
 	{
 		href: '/dashboard/conversations',
+		label: 'Conversations',
 		icon: <AiOutlineComment />,
 	},
 	{
 		href: '/dashboard/notes',
+		label: 'Notes',
 		icon: <AiOutlineBook />,
 	},
 	{
 		href: '/dashboard/teams',
+		label: 'Teams',
 		icon: <AiOutlineTeam />,
 	},
 	{
 		href: '/dashboard/calendar',
+		label: 'Calendar',
 		icon: <AiOutlineCalendar />,
 	},
 	{
 		href: '/dashboard/settings',
+		label: 'Settings',
 		icon: <AiOutlineSetting />,
 	},
 ];
@@ -34,10 +39,11 @@ const items = [
 export const DashboardNavigation = () => (
 	<nav className="w-14 border-r bg-white">
 		<ul className="space-y-1">
-			{items.map(({ href, icon }) => (
+			{items.map(({ href, label, icon }) => (
 				<li key={href}>
 					<ActiveLink
 						href={href}
+						aria-label={label}
 						className="flex aspect-square items-center justify-center border-l-2 border-transparent text-2xl text-gray-600 hover:text-indigo-600"
 						activeClassName="border-indigo-600 text-indigo-600"
 					>
