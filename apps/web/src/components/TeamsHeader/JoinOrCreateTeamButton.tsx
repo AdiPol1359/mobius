93a105d4773109c723b52c91b313023bb8324323
Adrian Polak <adrian.polak20@gmail.com>
2023-02-18 13:48:50 +0100

    feat(web): add create team modal (#21)

---
+++ apps/web/src/components/TeamsHeader/JoinOrCreateTeamButton.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { AiOutlineTeam } from 'react-icons/ai';
+
+import { useModal } from '@/hooks/useModal';
+
+import { Button } from '../Button';
+import { CreateTeamModal } from '../CreateTeamModal';
+import { JoinTeamModal } from '../JoinTeamModal';
+
+export const JoinOrCreateTeamButton = () => {
+	const {
+		isOpen: isJoinTeamOpen,
+		openModal: openJoinTeamModal,
+		closeModal: closeJoinTeamModal,
+	} = useModal();
+
+	const {
+		isOpen: isCreateTeamOpen,
+		openModal: openCreateTeamModal,
+		closeModal: closeCreateTeamModal,
+	} = useModal();
+
+	return (
+		<>
+			<Button type="button" onClick={openJoinTeamModal}>
+				<AiOutlineTeam className="text-xl" />
+				Join or create team
+			</Button>
+			<JoinTeamModal
+				isOpen={isJoinTeamOpen}
+				onClose={closeJoinTeamModal}
+				onButtonClick={openCreateTeamModal}
+			/>
+			<CreateTeamModal
+				isOpen={isCreateTeamOpen}
+				onClose={closeCreateTeamModal}
+			/>
+		</>
+	);
+};
