60b7ea54b1d631d9ba0e2be20b0b4a9dc4bb48a3
adipol1359 <adrian.polak20@gmail.com>
2023-02-21 13:36:41 +0100

    styles(common): remove enter

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -3,5 +3,4 @@ export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 export const PASSWORD_ERROR_MESSAGE =
 	'The password must be between 6 and 20 characters long and must contain at least one uppercase letter and one number';
-
 export const EMAIL_ERROR_MESSAGE = 'The e-mail address is invalid';
