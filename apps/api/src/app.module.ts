0abe5ef18418085d2d79fe576ace4c42569f835a
adipol1359 <adrian.polak20@gmail.com>
2023-02-24 14:29:04 +0100

    refactor(api): remove auth module from root module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [PrismaModule, UsersModule, SessionsModule, AuthModule],
+	imports: [PrismaModule, UsersModule, SessionsModule],
 })
 export class AppModule {}
