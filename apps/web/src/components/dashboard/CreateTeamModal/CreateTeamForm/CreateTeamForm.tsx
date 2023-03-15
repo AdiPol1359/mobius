ed00e45de6d9670ec18b3a2ce357c77293d1f9e6
adipol1359 <adrian.polak20@gmail.com>
2023-03-15 19:54:43 +0100

    feat(web): add toast to create team form

--- apps/web/src/components/dashboard/CreateTeamModal/CreateTeamForm/CreateTeamForm.tsx
+++ apps/web/src/components/dashboard/CreateTeamModal/CreateTeamForm/CreateTeamForm.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { toast } from 'react-hot-toast';
+
 import { Button } from '@/components/common/Button/Button';
 import { Input } from '@/components/common/Input/Input';
 import { useTeams } from '@/hooks/useTeams';
 import { useZodForm } from '@/hooks/useZodForm';
+import { DEFAULT_ERROR_MESSAGE } from '@/lib/constants';
 
 import { createTeamFormSchema } from './CreateTeamForm.schemas';
 
@@ -19,7 +22,7 @@ export const CreateTeamForm = ({ onSuccess }: CreateTeamFormProps) => {
 		formState: { errors },
 	} = useZodForm(createTeamFormSchema, {
 		onSubmit: ({ name }) => {
-			createTeamMutation.mutate(
+			const mutation = createTeamMutation.mutateAsync(
 				{ name },
 				{
 					onSuccess: ({ data }) => {
@@ -28,6 +31,12 @@ export const CreateTeamForm = ({ onSuccess }: CreateTeamFormProps) => {
 					},
 				}
 			);
+
+			toast.promise(mutation, {
+				loading: 'Creating a new team...',
+				success: ({ data: { name } }) => `Successfully created ${name} team!`,
+				error: DEFAULT_ERROR_MESSAGE,
+			});
 		},
 	});
 
