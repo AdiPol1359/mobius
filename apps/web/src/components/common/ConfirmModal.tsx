afde38087b8871c552eeaa15baa91452ddd3f89c
adipol1359 <adrian.polak20@gmail.com>
2023-03-26 11:33:05 +0200

    refactor(web): refactor to inline return

--- apps/web/src/components/common/ConfirmModal.tsx
+++ apps/web/src/components/common/ConfirmModal.tsx
@@ -13,18 +13,16 @@ export const ConfirmModal = ({
 	title,
 	onConfirm,
 	...props
-}: ConfirmModalProps) => {
-	return (
-		<Modal {...props}>
-			<Modal.Title>{title}</Modal.Title>
-			<Modal.Footer>
-				<Button type="button" onClick={props.onClose}>
-					Cancel
-				</Button>
-				<Button type="button" variant="primary" onClick={onConfirm}>
-					Confirm
-				</Button>
-			</Modal.Footer>
-		</Modal>
-	);
-};
+}: ConfirmModalProps) => (
+	<Modal {...props}>
+		<Modal.Title>{title}</Modal.Title>
+		<Modal.Footer>
+			<Button type="button" onClick={props.onClose}>
+				Cancel
+			</Button>
+			<Button type="button" variant="primary" onClick={onConfirm}>
+				Confirm
+			</Button>
+		</Modal.Footer>
+	</Modal>
+);
--- apps/web/src/components/dashboard/CreateTeamModal/CreateTeamModal.tsx
+++ apps/web/src/components/dashboard/CreateTeamModal/CreateTeamModal.tsx
@@ -4,11 +4,9 @@ import { Modal } from '@/components/common/Modal/Modal';
 
 import { CreateTeamForm } from './CreateTeamForm/CreateTeamForm';
 
-export const CreateTeamModal = (props: ComponentProps<typeof Modal>) => {
-	return (
-		<Modal {...props}>
-			<Modal.Title>Create your own team</Modal.Title>
-			<CreateTeamForm onSuccess={props.onClose} />
-		</Modal>
-	);
-};
+export const CreateTeamModal = (props: ComponentProps<typeof Modal>) => (
+	<Modal {...props}>
+		<Modal.Title>Create your own team</Modal.Title>
+		<CreateTeamForm onSuccess={props.onClose} />
+	</Modal>
+);
