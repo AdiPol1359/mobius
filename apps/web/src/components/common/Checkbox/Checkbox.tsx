58a7929f57ea1b5b952acb04a23f18ca46918abf
adipol1359 <adrian.polak20@gmail.com>
2023-03-29 19:57:57 +0200

    feat(web): add ref to checkbox

--- apps/web/src/components/common/Checkbox/Checkbox.tsx
+++ apps/web/src/components/common/Checkbox/Checkbox.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import { FaCheck } from 'react-icons/fa';
 
 import { ErrorMessage } from '../ErrorMessage';
@@ -5,17 +6,28 @@ import { ErrorMessage } from '../ErrorMessage';
 type CheckboxProps = Readonly<{
 	label?: string;
 	error?: string;
-}>;
+}> &
+	JSX.IntrinsicElements['input'];
 
-export const Checkbox = ({ label, error }: CheckboxProps) => (
-	<div className="space-y-0.5">
-		<label className="flex cursor-pointer items-center gap-x-2 text-sm">
-			<input type="checkbox" className="peer hidden" />
-			<div className="flex h-6 w-6 items-center justify-center rounded-md border bg-white text-white shadow-sm transition-colors duration-100 peer-checked:border-indigo-500 peer-checked:bg-indigo-600">
-				<FaCheck />
-			</div>
-			{label}
-		</label>
-		<ErrorMessage error={error} />
-	</div>
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+	({ label, error, ...props }, ref) => (
+		<div className="space-y-0.5">
+			<label className="flex cursor-pointer items-center gap-x-2 text-sm">
+				<input
+					ref={ref}
+					data-invalid={Boolean(error)}
+					type="checkbox"
+					className="peer hidden"
+					{...props}
+				/>
+				<div className="flex h-6 w-6 items-center justify-center rounded-md border bg-white text-white shadow-sm transition-colors duration-100 peer-checked:border-indigo-500 peer-checked:bg-indigo-600">
+					<FaCheck />
+				</div>
+				{label}
+			</label>
+			<ErrorMessage error={error} />
+		</div>
+	)
 );
+
+Checkbox.displayName = 'Checkbox';
