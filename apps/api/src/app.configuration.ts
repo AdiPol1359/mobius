ebe47eb0d5143f2837428796ab2f4a68c4a77bde
adipol1359 <adrian.polak20@gmail.com>
2023-04-06 18:25:50 +0200

    refactor: change zod schemas to nonempty

--- apps/api/src/app.configuration.ts
+++ apps/api/src/app.configuration.ts
@@ -4,17 +4,17 @@ import { TypeOf, z } from 'zod';
 const NUMBER_REGEX = /^\d+$/;
 
 const AppConfigSchema = z.object({
-	HOST: z.string().min(1),
+	HOST: z.string().nonempty(),
 	PORT: z.string().regex(NUMBER_REGEX).transform(Number),
 	SALT_OR_ROUNDS: z.union([
 		z.string().regex(NUMBER_REGEX).transform(Number),
 		z.string(),
 	]),
-	CORS_ORIGIN: z.string().min(1),
-	SESSION_COOKIE_NAME: z.string().min(1),
-	SESSION_COOKIE_SECRET: z.string().min(1),
-	SESSION_COOKIE_MAX_AGE: z.string().min(1),
-	REDIS_URL: z.string().min(1),
+	CORS_ORIGIN: z.string().nonempty(),
+	SESSION_COOKIE_NAME: z.string().nonempty(),
+	SESSION_COOKIE_SECRET: z.string().nonempty(),
+	SESSION_COOKIE_MAX_AGE: z.string().nonempty(),
+	REDIS_URL: z.string().nonempty(),
 });
 
 export const validate = (data: Record<string, unknown>) =>
--- apps/web/src/components/entry/SignUpForm/SignUpForm.schemas.ts
+++ apps/web/src/components/entry/SignUpForm/SignUpForm.schemas.ts
@@ -8,8 +8,8 @@ import { z } from 'zod';
 
 export const signUpFormSchema = z
 	.object({
-		firstName: z.string().trim().min(1, 'Please enter your first name'),
-		lastName: z.string().trim().min(1, 'Please enter your last name'),
+		firstName: z.string().trim().nonempty('Please enter your first name'),
+		lastName: z.string().trim().nonempty('Please enter your last name'),
 		email: z.string().trim().regex(EMAIL_REGEX, EMAIL_ERROR_MESSAGE),
 		password: z.string().regex(PASSWORD_REGEX, PASSWORD_ERROR_MESSAGE),
 		confirmPassword: z.string().regex(PASSWORD_REGEX, PASSWORD_ERROR_MESSAGE),
