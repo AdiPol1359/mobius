38d61ca5eb07f7f4851126cfcb748cc6ad2b223b
adipol1359 <aditv1337@gmail.com>
2023-04-23 16:02:20 +0200

    feat(api): add example values

--- apps/api/src/teams/members/dto/member.dto.ts
+++ apps/api/src/teams/members/dto/member.dto.ts
@@ -2,10 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 import { TeamMemberRole } from '@prisma/client';
 
 export class MemberDto {
+	/**
+	 * @example 1
+	 */
 	id: number;
 
+	/**
+	 * @example John
+	 */
 	firstName: string;
 
+	/**
+	 * @example Burton
+	 */
 	lastName: string;
 
 	@ApiProperty({ enum: TeamMemberRole, isArray: true })
